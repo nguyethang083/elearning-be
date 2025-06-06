b06cd015be39e86f03719e3fe54fc196f4c38c1c
Hang Vu <nguyethang083@gmail.com>
2025-06-06 22:09:13 +0700

    update 3

--- elearning/www/nextjs-app/next.config.js
+++ elearning/www/nextjs-app/next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // THÊM DÒNG NÀY ĐỂ TẮT TỐI ƯU HÓA HÌNH ẢNH
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http", // Thêm cho learn.local nếu bạn phục vụ ảnh từ Frappe qua Nginx
+        hostname: "math.local",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
